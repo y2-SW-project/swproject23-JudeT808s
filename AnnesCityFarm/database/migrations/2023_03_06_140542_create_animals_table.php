c114e9dc6bbeb370bbb429a027cd1f9c5dc3328f
jude <n00213344@iadt.ie>
2023-03-06 15:34:20 +0000

    Added tables

---
+++ AnnesCityFarm/database/migrations/2023_03_06_140542_create_animals_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('animals', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->int('age');
+            $table->string('description');
+            //One to Many
+            $table->type();
+            $table->image();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('animals');
+    }
+};
